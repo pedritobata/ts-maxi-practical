f4ecbd99eb0330d1d045ec067362c103a6c936f9
Pedro Martinez <pedromartinez_goyo@hotmail.com>
2020-01-08 19:07:01 -0500

    third psrty libraries

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -13,6 +13,9 @@
 
 
 
+
+
+
 //******* PARA IMPORTS TIPO ES6  ***************/
 //se debe importar el file CON LA EXTENSION .js, por ejemplo:
 // import { something } from "some-file.js;"
@@ -78,3 +81,21 @@ const finishedProjects = new ProjectList('finished');
 //clonados y adaptados a TS!! sus archivos tienen la extension .d.ts 
 //para lodash por ejemplo instalo:  npm install --save-dev @types/lodash
 //solo con instalar eso , mi import:  import _ from 'lodash'; ya funcionará!!! sin necesidad de noEmitOnError
+//si tenemos librerias que no tienen disponible su respectivo archivo .d.ts , o tenemos codigo js que
+//viene de otras fuentes como por ejemplo linkeados desde el html, podemos
+// usar la palabra reservada "declare".  Esta instruccion le dice a TS que la variable que
+//declare a continuacion existirá en el html final, a pesar que no es parte del bundle que arma
+//webpack.
+//por ejemplo , si en el html, en un script , declaro una variable y la quiero usar acá, uso
+//declare para que el compilador de TS no se queje. Al final en el html estará dicha variable y
+//el bundle con mi codigo la podrá utilizar
+declare var VAR_GLOBAL: any;
+console.log(VAR_GLOBAL);
+
+//Tambien existen librerias de terceros que estan escritas con TS, por ejemplo:
+// class-transformer  , esta libreria ayuda a transformar objetos js a instancias de una clase que queramos
+// osea, si consumimos una API , esta nos trae la data en json, pero esta data tiene la estructura
+//de un objeto pero NO es una instancia. class-transformer los vuelve instancias por nosotros!!
+
+//class-validator  , esta libreria nos sirve para validar atributos de instancias de clases que queramos
+//es muy similar al decorator que creamos antes en este curso, para validar!!!!
