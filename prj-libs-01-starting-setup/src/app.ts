6504bcaa129984a68c92f551df9080174dd46157
Pedro Martinez <pedromartinez_goyo@hotmail.com>
2020-01-09 08:58:15 -0500

    first commit - google geocoding account created

--- prj-libs-01-starting-setup/src/app.ts
+++ prj-libs-01-starting-setup/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyCeC6VSTSjx5AIQI8MjNSGTYy0zkkg1PXY';
+
+function searchAddressHandler(event: Event){
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+}
+
+form.addEventListener('submit',searchAddressHandler);
