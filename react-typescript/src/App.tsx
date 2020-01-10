7b91e47606eed8a61f03295800a8a5c8adb3c7c8
Pedro Martinez <pedromartinez_goyo@hotmail.com>
2020-01-10 09:46:27 -0500

    New Project react-typescript - creation, cleaning up, some files were removed and stuff

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//Hemos creado esta aplicacion usando:
+//  npx create-react-app <nombre de mi app> --typescript
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
