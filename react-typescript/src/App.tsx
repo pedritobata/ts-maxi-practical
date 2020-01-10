6b8701e7789f9d5a8ed1f2ae623687ec7c5e2deb
Pedro Martinez <pedromartinez_goyo@hotmail.com>
2020-01-10 10:21:27 -0500

    creating simple TodoList componente

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import './App.css';
+import TodoList from './components/TodoList';
 
 //Hemos creado esta aplicacion usando:
 //  npx create-react-app <nombre de mi app> --typescript
 
+//TS nos ayuda con clases creadas para React.js
+//Por ejemplo FC es una clase (atajo para FunctionComponent) que indica que nuestra constante
+//App es un functional component que React reconocerá
 const App: React.FC = () => {
+
+  const todos = [{id: 't1', title: 'Finish the course'}]
+
   return (
     <div className="App">
-      
+       <TodoList items={todos} />
     </div>
   );
 }
---
+++ react-typescript/src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface TodoListProps{
+    items: {id:string, title: string}[]
+}
+
+const TodoList: React.FC<TodoListProps> = props => {
+
+    return <ul>
+        {props.items.map(todo => <li key={todo.id}>{todo.title}</li>)}
+    </ul>
+}
+
+export default TodoList;
