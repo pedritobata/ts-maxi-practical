23e6e0a69cd0e8a8b60da8023fc4753545abef1c
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-06-17 10:27:56 -0500

    probando this en la clase Department

---
+++ practicing/src/2-clases.ts
@@ -0,0 +1,19 @@
+class Department{
+    name: string;
+
+    constructor(name: string){
+        this.name = name;
+        //this.describe = this.describe.bind(this);
+    }
+
+    describe(this: Department){//el parametro this obliga a que este metodo sea invocado por un Department, si no No compila!!
+        console.log('Department:', this.name);
+    }
+
+}
+
+const accounting = new Department('accounting');
+accounting.describe();
+
+const accountingCopy = { name:  'New accounting!!',describe: accounting.describe };
+accountingCopy.describe();
