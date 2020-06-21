9b92da23477af7bb84ba52990f40f1a664c9484a
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-06-21 10:05:01 -0500

    optional properties check

--- practicing/src/2-clases.ts
+++ practicing/src/2-clases.ts
@@ -1,13 +1,15 @@
 class Department{
-    name: string;
+    name?: string;
+    description?: string;
 
-    constructor(name: string){
+    constructor(name?: string, description?: string){
         this.name = name;
         //this.describe = this.describe.bind(this);
     }
 
     describe(this: Department){//el parametro this obliga a que este metodo sea invocado por un Department, si no No compila!!
         console.log('Department:', this.name);
+        console.log('Description:', this.description);
     }
 
 }
@@ -17,3 +19,5 @@ accounting.describe();
 
 const accountingCopy = { name:  'New accounting!!',describe: accounting.describe };
 accountingCopy.describe();
+const accounting2 = new Department();
+accounting2.describe();
